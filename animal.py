498561abc1ba28817ef558f3796a7b713656a174
david <novelo0david@gmail.com>
2021-04-01 00:00:26 -0700

    initial submission

---
+++ animal.py
@@ -0,0 +1,7 @@
+# author: David Novelo
+# Date: 03/31/2021
+# Description: asks the user for their favorite animal and then
+# prints out that that is their favorite animal
+
+fave_animal = input("Please enter your favorite animal:")
+print("your favorite animal is the " + fave_animal +".")
